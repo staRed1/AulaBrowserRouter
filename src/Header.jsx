7d280c02d6c513f032f1d328c98255a3830aeae8
staRed1 <joaoarthur.prates@gmail.com>
2024-08-20 15:41:58 -0400

    trabalho
    SOCORRO!!!

---
+++ src/Header.jsx
@@ -0,0 +1,3 @@
+import {Link}
+
+export default function
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}/>
+      </Routes>
+      </BrowserRouter>
   </React.StrictMode>,
 )
 
